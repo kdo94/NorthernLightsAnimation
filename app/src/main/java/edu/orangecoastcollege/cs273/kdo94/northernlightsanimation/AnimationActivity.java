6b2c2c1a840ba37bdd71f58d8e762540e9940c5b
kdo94 <kdo94@student.cccd.edu>
2016-10-04 15:21:34 -0700

    Explore animations used to control the visual appearance of views in our user interface.  We'll make use of the Frame and Tween styles of animation to give extra interest to this app.

--- app/src/main/java/edu/orangecoastcollege/cs273/kdo94/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/northernlightsanimation/AnimationActivity.java
@@ -9,6 +9,5 @@ public class AnimationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
-        //asetasdfa
     }
 }
